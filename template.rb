07f3bc38bc9d32895c43b3af850d2caed8f3842d
vic778 <victoiremmanuelbarh@gmail.com>
2023-01-12 17:13:48 +0400

    Create template class

--- template.rb
+++ template.rb
@@ -1,16 +1,15 @@
-require_relative './question.rb'
+require_relative './question'
 
 class Template < Question
-   
-    q1 = "What color are apples (a)red (b)black  (c)orange"
-    q2 = "what color are bananas? (a)ping (b)red (c)yellow"
-    q3 = "Who's the creator of this algorith? (a)chrisian (b)victor (c)chanel"
+  q1 = 'What color are apples (a)red (b)black  (c)orange'
+  q2 = 'what color are bananas? (a)ping (b)red (c)yellow'
+  q3 = "Who's the creator of this algorith? (a)chrisian (b)victor (c)chanel"
 
-    questions = [
-        Question.new(q1, "a"),
-        Question.new(q2, "c"),
-        Question.new(q3, "b")
-    ]   
-   
-    run_test(questions)
+  questions = [
+    Question.new(q1, 'a'),
+    Question.new(q2, 'c'),
+    Question.new(q3, 'b')
+  ]
+
+  run_test(questions)
 end
